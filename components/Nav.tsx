4f8d4b4504bfe7c144d706480dcac5de4160ddd3
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-08-07 22:24:08 +0100

    hide blog and EC links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import styles from "../styles/Nav.module.css";
 
 const Nav = () => {
+  const router = useRouter();
+  const check = router.asPath.includes("/blog");
   return (
     <nav className={styles.section}>
       <div className="container">
@@ -15,12 +18,16 @@ const Nav = () => {
             <Link href={"/"}>
               <a className={styles.navLink}>Home</a>
             </Link>
-            <Link href={"#blogScetion"}>
-              <a className={styles.navLink}>Blog</a>
-            </Link>
-            <Link href={"#editorsChoiceSection"}>
-              <a className={styles.navLink}>Editor&apos;s Choice</a>
-            </Link>
+            {!check && (
+              <Link href={"#blogScetion"}>
+                <a className={styles.navLink}>Blog</a>
+              </Link>
+            )}
+            {!check && (
+              <Link href={"#editorsChoiceSection"}>
+                <a className={styles.navLink}>Editor&apos;s Choice</a>
+              </Link>
+            )}
           </div>
         </div>
       </div>
